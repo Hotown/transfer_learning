11e8a6a66c578b30f2fcc1dc4bdb107223288d10
Hotown <hotown0515@163.com>
2020-10-14 10:28:52 +0800

    add TCA keynote

--- code/traditional/TCA.py
+++ code/traditional/TCA.py
@@ -5,24 +5,37 @@ import scipy.io as sio
 from sklearn.neighbors import KNeighborsClassifier
 
 
-def kernal(X1, X2):
+def kernal(type, X1, X2, gamma):
     '''
 
+    :param type: linear or rbg
     :param X1:
     :param X2:
+    :param gamma: the parm of rbg
     :return:
     '''
+
     K = None
-    if X2 is not None:
-        K = sklearn.metrics.pairwise.linear_kernel(np.asarray(X1).T, np.asarray(X2).T)
-    else:
-        K = sklearn.metrics.pairwise.linear_kernel(np.asarray(X1).T)
+    if not type:
+        return X1
+
+    elif type == 'linear':
+        if X2 is not None:
+            K = sklearn.metrics.pairwise.linear_kernel(np.asarray(X1).T, np.asarray(X2).T)
+        else:
+            K = sklearn.metrics.pairwise.linear_kernel(np.asarray(X1).T)
+
+    elif type == 'rbf':
+        if X2 is not None:
+            K = sklearn.metrics.pairwise.rbf_kernel(np.asarray(X1).T, np.asarray(X2).T, gamma)
+        else:
+            K = sklearn.metrics.pairwise.rbf_kernel(np.asarray(X1).T, None, gamma)
 
     return K
 
 
 class TCA:
-    def __init__(self, dim=30, lamb=1):
+    def __init__(self, dim=30, lamb=1, gamma=1):
         '''
 
         :param dim: 降维后的维度
@@ -30,6 +43,7 @@ class TCA:
         '''
         self.dim = dim
         self.lamb = lamb
+        self.gamma = gamma
 
     def fit(self, Xs, Xt):
         '''
@@ -38,15 +52,14 @@ class TCA:
         :return:
         '''
         X = np.hstack((Xs.T, Xt.T))
-        X = X / np.linalg.norm(X, axis=0)  # 归一化
+        X = X / np.linalg.norm(X, axis=0)
         m, n = X.shape
         ns, nt = len(Xs), len(Xt)
         e = np.vstack((1 / ns * np.ones((ns, 1)), -1 / nt * np.ones((nt, 1))))
         L = e * e.T
-        L = L / np.linalg.norm(L, 'fro')
         H = np.eye(n) - 1 / n * np.ones((n, n))
-        K = kernal(X, None)
-
+        # K = kernal('linear', X, None, self.gamma)
+        K= kernal('rbf', X, None, gamma=self.gamma)
         Sa = np.linalg.multi_dot([K, L, K.T]) + self.lamb * np.eye(n)
         Sb = np.linalg.multi_dot([K, H, K.T])
 
@@ -83,6 +96,6 @@ if __name__ == '__main__':
                 src, tar = '../data/surf/' + domains[i], '../data/surf/' + domains[j]
                 src_domain, tar_domain = sio.loadmat(src), sio.loadmat(tar)
                 Xs, Ys, Xt, Yt = src_domain['fts'], src_domain['labels'], tar_domain['fts'], tar_domain['labels']
-                tca = TCA(dim=30, lamb=1)
+                tca = TCA(dim=50, lamb=1, gamma=1)
                 acc, y_ = tca.train(Xs, Xt, Ys, Yt)
                 print('Acc:{:4f}'.format(acc))
